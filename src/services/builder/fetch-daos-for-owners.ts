5f1b414306881ffea17366f869c143b309685938
Milad Nekofar <milad@nekofar.com>
2024-10-09 16:28:45 +0400

    feat(services): add `getDAOsForOwners` to fetch DAO data
    Added a new service to fetch DAO details for given owner addresses across multiple subgraph endpoints. This helps in aggregating and returning unique DAOs.

---
+++ src/services/builder/fetch-daos-for-owners.ts
@@ -0,0 +1,73 @@
+import { Env } from '@/services/builder/types'
+import { gql, GraphQLClient } from 'graphql-request'
+import { flatMap, pipe, uniqueBy } from 'remeda'
+import { JsonObject } from 'type-fest'
+
+export interface Dao {
+  id: string
+  name: string
+}
+
+interface DaoTokenOwner {
+  id: string
+  owner: string
+  dao: Dao
+  daoTokenCount: number
+}
+
+type Data = {
+  daotokenOwners: DaoTokenOwner[]
+} & JsonObject
+
+interface Result {
+  daos: Dao[]
+}
+
+export const getDAOsForOwners = async (
+  env: Env,
+  ownerAddresses: string[],
+): Promise<Result> => {
+  const query = gql`
+    {
+      daotokenOwners(
+        skip: 0
+        first: 1000
+        where: { owner_in: ${JSON.stringify(ownerAddresses)} }
+      ) {
+        id
+        owner
+        dao {
+          id
+          name
+        }
+        daoTokenCount
+      }
+    }
+  `
+
+  const endpoints = [
+    env.BUILDER_SUBGRAPH_ETHEREUM_URL,
+    env.BUILDER_SUBGRAPH_BASE_URL,
+    env.BUILDER_SUBGRAPH_OPTIMISM_URL,
+    env.BUILDER_SUBGRAPH_ZORA_URL,
+  ]
+
+  try {
+    const daoPromises = endpoints.map(async (endpoint) => {
+      const client = new GraphQLClient(endpoint)
+      const response = await client.request<Data>(query)
+      return response.daotokenOwners.map((owner) => owner.dao)
+    })
+
+    const results = await Promise.all(daoPromises)
+    const uniqueDaos = pipe(
+      results,
+      flatMap((daos) => daos),
+      uniqueBy((dao) => dao.id),
+    )
+    return { daos: uniqueDaos }
+  } catch (error) {
+    console.error('Error fetching DAO token owners:', error)
+    throw error
+  }
+}
