95be4247507599d045bf070862a926df98e42795
Milad Nekofar <milad@nekofar.com>
2024-10-28 16:15:56 +0400

    refactor(services): restructure `getDAOsTokenOwners` pagination logic
    Simplified the pagination logic in `getDAOsTokenOwners` by removing unnecessary functions and variables. Enhanced the code clarity and efficiency by iterating over endpoints and handling pagination within a single loop.

--- src/services/builder/get-daos-token-owners.ts
+++ src/services/builder/get-daos-token-owners.ts
@@ -1,33 +1,46 @@
 import { Env, Owner } from '@/services/builder/types'
 import { gql, GraphQLClient } from 'graphql-request'
-import { flatMap, pipe } from 'remeda'
 import { JsonObject } from 'type-fest'
 
 type Data = {
-  owners: Owner[]
+  owners: Owner[] | null
 } & JsonObject
 
 interface Result {
   owners: Owner[]
 }
 
+/**
+ * Fetches DAO token owners from multiple subgraphs.
+ *
+ * This asynchronous function queries several subgraph endpoints -- Ethereum, Base, Optimism, and Zora --
+ * to retrieve and aggregate a list of all DAO token owners.
+ * @param env - The environment object containing subgraph URLs.
+ * @param [skip] - The number of records to skip (for pagination).
+ * @param [first] - The number of records to retrieve (for pagination).
+ * @returns A promise that resolves to a result object containing an array of DAO token owners.
+ * @throws Will throw an error if the subgraph requests fail.
+ */
 export const getDAOsTokenOwners = async (
   env: Env,
   skip = 0,
   first = 10,
 ): Promise<Result> => {
   let allOwners: Owner[] = []
-  let currentSkip = skip || 0
-  let hasMore: boolean
+  const endpoints = [
+    env.BUILDER_SUBGRAPH_ETHEREUM_URL,
+    env.BUILDER_SUBGRAPH_BASE_URL,
+    env.BUILDER_SUBGRAPH_OPTIMISM_URL,
+    env.BUILDER_SUBGRAPH_ZORA_URL,
+  ]
 
   const query = gql`
-    {
+    query GetDAOTokenOwners($skip: Int!, $first: Int!) {
       owners: daotokenOwners(
-        skip: ${currentSkip}
-        first: ${first}
+        skip: $skip
+        first: $first
         orderBy: daoTokenCount
         orderDirection: desc
-        where: {}
         subgraphError: deny
       ) {
         id
@@ -41,35 +54,25 @@ export const getDAOsTokenOwners = async (
     }
   `
 
-  const endpoints = [
-    env.BUILDER_SUBGRAPH_ETHEREUM_URL,
-    env.BUILDER_SUBGRAPH_BASE_URL,
-    env.BUILDER_SUBGRAPH_OPTIMISM_URL,
-    env.BUILDER_SUBGRAPH_ZORA_URL,
-  ]
-
   try {
-    do {
-      const responses = await Promise.all(
-        endpoints.map(async (endpoint) => {
-          const client = new GraphQLClient(endpoint)
-          const response = await client.request<Data>(query)
-          return response.owners
-        }),
-      )
-
-      const combinedResults = pipe(
-        responses,
-        flatMap((result) => result),
-      )
-
-      allOwners = [...allOwners, ...combinedResults]
-      hasMore = allOwners.length > 0
-
-      if (hasMore) {
-        currentSkip += combinedResults.length
+    for (const endpoint of endpoints) {
+      let currentSkip = skip
+      let hasMore = true
+      while (hasMore) {
+        const client = new GraphQLClient(endpoint)
+        const response = await client.request<Data>(query, {
+          skip: currentSkip,
+          first,
+        })
+        const owners = response.owners ?? []
+        if (owners.length === 0) {
+          hasMore = false
+        } else {
+          allOwners = [...allOwners, ...owners]
+          currentSkip += owners.length
+        }
       }
-    } while (hasMore)
+    }
 
     return { owners: allOwners }
   } catch (error) {
